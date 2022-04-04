a09a4dc1094314e4c3ec74ae523a8f345868b87a
Ivan-Krul <kiwiivanya128@gmail.com>
2022-04-04 11:34:45 +0300

    made input and output

--- DiaryBook/Main.cpp
+++ DiaryBook/Main.cpp
@@ -1,7 +1,29 @@
 ﻿#include <iostream>
 #include "Page.h"
 
+using std::cout;
+using std::cin;
+using std::getline;
+using std::string;
+
+string INPUT;
+unsigned int Page::NPage = 0;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Page page;
+
+    cout << "Title: ";
+    getline(cin,INPUT);
+    page.writeTitle(INPUT);
+
+    cout << "Text: ";
+    getline(cin, INPUT);
+    page.writeText(INPUT);
+
+    cout << '\n';
+    cout << page.readTitle();
+    cout << '\n';
+    cout << page.readText();
+    cout << '\n';
 }
